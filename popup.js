11ab7e315adeabf66efd104300e9705a9d5a93b4
Jacob Broughton <jlbroughton88@gmail.com>
2024-02-01 12:40:17 -0500

    now uses local storage, style updates

--- popup.js
+++ popup.js
@@ -1,40 +1,53 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const controls = document.querySelector(".controls");
-  const newTextButton = document.querySelector(".new-text-button");
-  const saveButton = document.querySelector(".save-button");
-  const textsContainer = document.querySelector(".texts-container");
+  const newTextButton = document.querySelector(".new-note-button");
+  const exportButton = document.querySelector(".export-button");
+  const textsContainer = document.querySelector(".notes-container");
   const fileInput = document.querySelector(".file-input");
 
-  let texts = [];
-  let lastTextId = texts[texts.length - 1]?.id || -1;
+  const localStorageData = localStorage.getItem("textLib");
 
-  // * New Text
+  let notes = [];
+
+  if (localStorageData !== null && localStorageData !== undefined) {
+    notes = JSON.parse(localStorageData);
+  }
+
+  let lastTextId = notes[notes.length - 1]?.id || -1;
+
+  refreshTexts(notes);
+
+  // * New note
   newTextButton.addEventListener("click", () => {
     const newTextId = (lastTextId += 1);
 
+    const noteContainer = document.createElement("div");
     const newTextArea = document.createElement("textarea");
     newTextArea.setAttribute("id", newTextId);
     newTextArea.addEventListener("change", (e) => {
-      console.log(e.target.id);
-      console.log(newTextId, e.target.value);
-      texts = texts.map((text) => ({
-        ...text,
-        ...(e.target.id == text.id && { content: e.target.value }),
+      notes = notes.map((note) => ({
+        ...note,
+        ...(e.target.id == note.id && { content: e.target.value }),
       }));
+      localStorage.setItem("textLib", JSON.stringify(notes));
     });
-    textsContainer.prepend(newTextArea);
+
+    const trashButton = createTrashButton(newTextId);
+
+    noteContainer.append(newTextArea);
+    textsContainer.prepend(noteContainer);
     newTextArea.focus();
 
-    texts.unshift({ id: newTextId, content: "" });
+    console.log(notes);
+    notes.unshift({ id: newTextId, content: "" });
   });
 
-  // * Save
-  saveButton.addEventListener("click", () => {
+  // * Export
+  exportButton.addEventListener("click", () => {
     const downloadLink = document.createElement("a");
 
-    const content = texts || [];
+    const content = notes || [];
 
-    const file = new Blob([JSON.stringify(content)], { type: "text/plain" });
+    const file = new Blob([JSON.stringify(content)], { type: "note/plain" });
 
     downloadLink.href = URL.createObjectURL(file);
     downloadLink.download = "textlib.txt";
@@ -48,7 +61,7 @@ document.addEventListener("DOMContentLoaded", function () {
       const file = e.target.files[0];
 
       const reader = new FileReader();
-``
+
       reader.onload = function (e) {
         const content = e.target.result;
         JSON.parse(content);
@@ -63,12 +76,28 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // * Refresh
   function refreshTexts(newTexts) {
-    texts = newTexts;
-    texts.forEach((text) => {
+    notes = newTexts;
+    notes.forEach((note) => {
       const textarea = document.createElement("textarea");
-      textarea.setAttribute("id", text.id);
-      textarea.innerText = text.content;
+      textarea.setAttribute("id", note.id);
+      textarea.innerText = note.content;
+
+      const trashButton = createTrashButton(note.id);
+
       textsContainer.append(textarea);
     });
   }
+
+  function createTrashButton(dataId) {
+    const trashButton = document.createElement("button");
+    trashButton.setAttribute("class", "trash-button");
+    trashButton.dataset.dataId = dataId;
+    trashButton.innerText = "Trash";
+    return trashButton;
+  }
 });
+
+// TODO - add "overwrite or append" when importing a file
+// TODO - add delete button to each note
+// TODO - add copy functionality to each note
+// TODO - add export functionality
