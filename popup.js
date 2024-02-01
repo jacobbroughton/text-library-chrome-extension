9ec4bed1b1c7e695fdb4ef40d6546ff91dd84058
Jacob Broughton <jlbroughton88@gmail.com>
2024-01-31 22:29:58 -0500

    successfully modifying items and saving to file

--- popup.js
+++ popup.js
@@ -1,13 +1,64 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // popup logic goes here
-  const button = document.querySelector("button");
-  const clickedCountSpan = document.querySelector("#clickedCount");
-  let clickedCount = 0;
+  const controls = document.querySelector(".controls");
+  const newTextButton = document.querySelector(".new-text-button");
+  const saveButton = document.querySelector(".save-button");
+  const textsContainer = document.querySelector(".texts-container");
+  const fileInput = document.querySelector(".file-input");
 
-  button.addEventListener("click", () => {
-    clickedCount += 1;
-    clickedCountSpan.innerText = clickedCount;
+  let texts = [];
+  let lastTextId = texts[texts.length - 1]?.id || -1;
+
+  newTextButton.addEventListener("click", () => {
+    const newTextId = (lastTextId += 1);
+
+    const newTextArea = document.createElement("textarea");
+    newTextArea.setAttribute("id", newTextId);
+    newTextArea.addEventListener("change", (e) => {
+      console.log(e.target.id);
+      console.log(newTextId, e.target.value);
+      texts = texts.map((text) => ({
+        ...text,
+        ...(e.target.id == text.id && { content: e.target.value }),
+      }));
+    });
+    textsContainer.prepend(newTextArea);
+    newTextArea.focus();
+
+    texts.unshift({ id: newTextId, content: "" });
   });
 
-  clickedCountSpan.innerText = clickedCount;
+  saveButton.addEventListener("click", () => {
+    const downloadLink = document.createElement("a");
+
+    const content = texts || [];
+
+    const file = new Blob([JSON.stringify(content)], { type: "text/plain" });
+
+    downloadLink.href = URL.createObjectURL(file);
+    downloadLink.download = "textlib.txt";
+    downloadLink.click();
+    URL.revokeObjectURL(downloadLink.href);
+  });
+
+  // fileInput.addEventListener("change", (e) => {
+  //   if (e.target.files[0]) {
+  //     const file = e.target.files[0]
+
+  //     const reader = new FileReader()
+
+  //     reader.onload = function(e) {
+  //       const content = e.target.result
+  //       // this is where i could use the content of the file
+  //       alert(content.toString())
+  //     }
+
+  //     // const useThisFileButton = document.createElement("button");
+  //     // useThisFileButton.innerText = "Use This File";
+  //     // controls.append(useThisFileButton);
+  //     // console.log(controls)
+  //     // alert(e.target.files[0].name);
+  //   } else {
+  //     alert("No file selected")
+  //   }
+  // });
 });
