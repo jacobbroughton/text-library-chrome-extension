047327f079544fd2fa33ef486701f6b8d7f24fea
Jacob Broughton <jlbroughton88@gmail.com>
2024-02-11 08:16:08 -0500

    mostly style changes

--- popup.js
+++ popup.js
@@ -1,45 +1,95 @@
+// TODO - (MVP) - add copy functionality to each note
+// TODO - (MVP) - add favicon logo
+// TODO - (MVP) - tell users where exported files are by default (find a common path that everyone would have)
+// TODO - (NOT MVP) add "overwrite or append" when importing a file
+// TODO - (NOT MVP) - add export prompt after x amount of changes without one (keep track)
+// TODO - (NOT MVP) - add history section that stays in localstorage and shows in scrollable window on bottom
+
 document.addEventListener("DOMContentLoaded", function () {
-  const newTextButton = document.querySelector(".new-note-button");
+  const newNoteButton = document.querySelector(".new-note-button");
   const exportButton = document.querySelector(".export-button");
-  const textsContainer = document.querySelector(".notes-container");
   const fileInput = document.querySelector(".file-input");
+  let notesContainer = null;
 
   const localStorageData = localStorage.getItem("textLib");
 
   let notes = [];
+  let currentView = "All";
 
   if (localStorageData !== null && localStorageData !== undefined) {
     notes = JSON.parse(localStorageData);
   }
 
-  let lastTextId = notes[notes.length - 1]?.id || -1;
+  let lastNoteId = notes[notes.length - 1]?.id || -1;
 
-  refreshTexts(notes);
+  if (!notesContainer) createNotesContainer();
+
+  refreshNotes(notes);
+
+  function handleNewNoteClick(e) {
+    // const newNoteId = lastNoteId + 1;
+    const notesCopy = [...notes];
+    const newNoteId = notesCopy[notesCopy.length - 1]?.id + 1 || 0;
+
+    // console.log({ notesCopy, newNoteId });
+    // const noteContainer = createNoteContainer(notes, newNoteId);
+
+    // const newTextArea = createTextArea(notes, newNoteId);
+    // newTextArea.innerText = newNoteId;
+
+    // noteContainer.append(newTextArea);
+    // notesContainer.prepend(noteContainer);
+    // newTextArea.focus();
+
+    console.log({ lastNoteId, newNoteId });
+    lastNoteId += 1;
+
+    let updatedNotes = [
+      ...notes,
+      {
+        id: newNoteId,
+        content: "",
+        inTrash: false,
+        createdDttm: getDateTime(),
+        modifiedDttm: null,
+      },
+    ];
+    console.log(updatedNotes);
+    refreshNotes(updatedNotes);
+  }
 
   // * New note
-  newTextButton.addEventListener("click", () => {
-    const newTextId = (lastTextId += 1);
+  newNoteButton.addEventListener("click", handleNewNoteClick);
 
-    const noteContainer = document.createElement("div");
-    const newTextArea = document.createElement("textarea");
-    newTextArea.setAttribute("id", newTextId);
-    newTextArea.addEventListener("change", (e) => {
-      notes = notes.map((note) => ({
-        ...note,
-        ...(e.target.id == note.id && { content: e.target.value }),
-      }));
-      localStorage.setItem("textLib", JSON.stringify(notes));
-    });
+  // * Delete
+  function createDeleteButton(dataId) {
+    const trashButton = document.createElement("button");
+    trashButton.classList.add("delete-button");
+    trashButton.dataset.dataId = dataId;
 
-    const trashButton = createTrashButton(newTextId);
+    function handleDeleteClick(e) {
+      e.stopPropagation();
 
-    noteContainer.append(newTextArea);
-    textsContainer.prepend(noteContainer);
-    newTextArea.focus();
+      let updatedNotes = [
+        ...notes.map((note) => ({
+          ...note,
+          ...(note.id == dataId && { inTrash: true }),
+        })),
+      ];
 
-    console.log(notes);
-    notes.unshift({ id: newTextId, content: "" });
-  });
+      // localStorage.setItem("textLib", JSON.stringify(notes));
+      refreshNotes(updatedNotes);
+
+      // for (const node of notesContainer.children) {
+      //   if (node.dataset.id == dataId.Id) {
+      //     notesContainer.removeChild(node);
+      //   }
+      // }
+    }
+
+    trashButton.addEventListener("click", handleDeleteClick);
+    return trashButton;
+  }
 
   // * Export
   exportButton.addEventListener("click", () => {
@@ -55,8 +105,59 @@ document.addEventListener("DOMContentLoaded", function () {
     URL.revokeObjectURL(downloadLink.href);
   });
 
-  // * Import
-  fileInput.addEventListener("change", (e) => {
+  // * Create note container
+  function createNoteContainer(newestNotes, noteId) {
+    // const newNoteId = lastNoteId + 1;
+    const noteContainer = document.createElement("div");
+    noteContainer.classList.add("note-container");
+    noteContainer.dataset.dataId = noteId;
+
+    function handleNoteClick(e) {
+      e.stopPropagation();
+      const correspondingNote = newestNotes.find((note) => note.id == noteId);
+      // if (correspondingNote) copyToClipboard(correspondingNote.content);
+    }
+
+    noteContainer.addEventListener("click", handleNoteClick);
+
+    return noteContainer;
+  }
+
+  // * Create text area
+  function createTextArea(newestNotes, newNoteId) {
+    const newTextArea = document.createElement("textarea");
+    newTextArea.setAttribute("id", newNoteId);
+
+    function handleChange(e) {
+      console.log("before updatedNotes", newestNotes);
+      let updatedNotes = [
+        ...newestNotes.map((note) => ({
+          ...note,
+          ...(e.target.id == note.id && {
+            content: e.target.value,
+            modifiedDttm: getDateTime(),
+          }),
+        })),
+      ];
+      console.log("after updatedNotes", updatedNotes);
+
+      // localStorage.setItem("textLib", JSON.stringify(notes));
+      refreshNotes(updatedNotes);
+    }
+
+    newTextArea.addEventListener("change", handleChange);
+
+    return newTextArea;
+  }
+
+  // * Create notes container
+  function createNotesContainer() {
+    notesContainer = document.createElement("div");
+    notesContainer.classList.add("notes-container", "section");
+    document.body.append(notesContainer);
+  }
+
+  function handleFileInput(e) {
     if (e.target.files[0]) {
       const file = e.target.files[0];
 
@@ -64,40 +165,80 @@ document.addEventListener("DOMContentLoaded", function () {
 
       reader.onload = function (e) {
         const content = e.target.result;
-        JSON.parse(content);
-        refreshTexts(JSON.parse(content));
+        refreshNotes(JSON.parse(content));
       };
 
       reader.readAsText(file);
     } else {
       alert("No file selected");
     }
-  });
+  }
+
+  // * Import
+  fileInput.addEventListener("change", handleFileInput);
 
   // * Refresh
-  function refreshTexts(newTexts) {
-    notes = newTexts;
-    notes.forEach((note) => {
-      const textarea = document.createElement("textarea");
-      textarea.setAttribute("id", note.id);
-      textarea.innerText = note.content;
+  function refreshNotes(updatedNotes) {
+    notes = updatedNotes;
+    lastNoteId = notes[notes.length - 1]?.id || -1;
 
-      const trashButton = createTrashButton(note.id);
+    if (notes[0] && !notesContainer) createNotesContainer();
+    console.log(notesContainer.children, notes);
+    while (notesContainer.firstChild) {
+      notesContainer.removeChild(notesContainer.firstChild);
+    }
 
-      textsContainer.append(textarea);
+    console.log(notesContainer.children);
+
+    const filteredNotes = notes.filter((note) => {
+      if (currentView === "All") {
+        if (note.inTrash) return false;
+      }
+
+      return true;
     });
+
+    filteredNotes.forEach((note) => {
+      const noteContainer = createNoteContainer(notes, note.id);
+      const textarea = createTextArea(notes, note.id);
+      textarea.innerText = note.content;
+      noteContainer.append(textarea);
+
+      const deleteButton = createDeleteButton(note.id);
+      const trashSVG = document.createElement("img");
+      trashSVG.src = "trash.svg";
+      // trashSVG.alt = 'Delete'
+      deleteButton.append(trashSVG);
+
+      noteContainer.append(deleteButton);
+
+      notesContainer.append(noteContainer);
+    });
+
+    localStorage.setItem("textLib", JSON.stringify(updatedNotes));
   }
 
-  function createTrashButton(dataId) {
-    const trashButton = document.createElement("button");
-    trashButton.setAttribute("class", "trash-button");
-    trashButton.dataset.dataId = dataId;
-    trashButton.innerText = "Trash";
-    return trashButton;
+  async function copyToClipboard(text) {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => console.log("Copied text to clipboard"))
+      .catch((err) => console.log(err));
+  }
+
+  function getDateTime() {
+    const currentDate = new Date();
+    const dateOptions = { day: "2-digit", month: "2-digit", year: "numeric" };
+    const timeOptions = {
+      hour12: false,
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      fractionalSecondDigits: 3,
+    };
+
+    const dateString = currentDate.toLocaleDateString("en-US", dateOptions);
+    const timeString = currentDate.toLocaleTimeString("en-US", timeOptions);
+
+    return `${dateString} ${timeString}`;
   }
 });
-
-// TODO - add "overwrite or append" when importing a file
-// TODO - add delete button to each note
-// TODO - add copy functionality to each note
-// TODO - add export functionality
